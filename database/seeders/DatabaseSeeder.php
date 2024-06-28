12a828932950412a47f910d0eb30d4f21ea8d119
RifqyYR <yudhiestrarifqy@gmail.com>
2024-06-28 14:37:15 +0800

    feat: add image model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
             'uuid' => Uuid::uuid4(),
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => env('DEFAULT_PASSWORD'),
+            'password' => Hash::make(env('DEFAULT_PASSWORD')),
         ]);
 
         DB::table('clients')->insert([
@@ -39,7 +39,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'rifqy@gmail.com',
             'phone_number' => '0895800859692',
             'address' => 'Jl. Poros Malino',
-            'password' => env('DEFAULT_PASSWORD'),
+            'password' => Hash::make(env('DEFAULT_PASSWORD')),
         ]);
 
         DB::table('spare_parts')->insert([
@@ -58,6 +58,13 @@ class DatabaseSeeder extends Seeder
             'uuid' => Uuid::uuid4(),
             'name' => 'Toyota Veloz 2024',
             'type' => 'NEW',
+            'description' => '<p>Toyota Veloz 2024 adalah 7 Seater MPV yang tersedia dalam daftar harga Rp&nbsp;304,4 Juta di Indonesia. It is available in 4 variants, 1 engine, and 2 transmissions option: Manual dan CVT in the Indonesia. Mobil ini memiliki ground clearance 205 mm dengan dimensi sebagai berikut: 4475 mm L x 1750 mm W x 1700 mm H. Lebih dari 99 pengguna telah memberikan penilaian untuk Veloz berdasarkan fitur, jarak tempuh, kenyamanan tempat duduk dan kinerja mesin. Pesaing terdekat Toyota Veloz adalah Xpander, Ertiga, Avanza dan Livina . Cicilan bulanan terendah dimulai dari Rp&nbsp;14,44 Juta (selama 60 bulan).</p>',
+        ]);
+
+        DB::table('catalog_cars')->insert([
+            'uuid' => Uuid::uuid4(),
+            'name' => 'Toyota Fortuner 2020',
+            'type' => 'USED',
             'description' => '<p>Toyota Veloz 2024 adalah 7 Seater MPV yang tersedia dalam daftar harga Rp&nbsp;304,4 Juta di Indonesia. It is available in 4 variants, 1 engine, and 2 transmissions option: Manual dan CVT in the Indonesia. Mobil ini memiliki ground clearance 205 mm dengan dimensi sebagai berikut: 4475 mm L x 1750 mm W x 1700 mm H. Lebih dari 99 pengguna telah memberikan penilaian untuk Veloz berdasarkan fitur, jarak tempuh, kenyamanan tempat duduk dan kinerja mesin. Pesaing terdekat Toyota Veloz adalah Xpander, Ertiga, Avanza dan Livina . Cicilan bulanan terendah dimulai dari Rp&nbsp;14,44 Juta (selama 60 bulan).</p>',
         ]);
 
@@ -108,6 +115,12 @@ class DatabaseSeeder extends Seeder
             'status' => 'WAITING',
             'client_id' => 1,
             'client_car_id' => 1
+        ]);
+
+        DB::table('images')->insert([
+            'uuid' => Uuid::uuid4(),
+            'img_url' => 'toyota-veloz.webp',
+            'catalog_car_id' => 1.
         ]);
     }
 }

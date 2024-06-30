787fc535478eefd4adb0e4841bc0759cd9e6035f
RifqyYR <yudhiestrarifqy@gmail.com>
2024-06-30 22:07:12 +0800

    feat: add book and visit service API

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -9,6 +9,13 @@ class Service extends Model
 {
     use HasFactory;
 
+    const TYPE_BOOK = 'BOOK';
+    const TYPE_VISIT = 'VISIT';
+    const STATUS_WAITING = 'WAITING';
+    const STATUS_CONFIRMED = 'CONFIRMED';
+    const STATUS_CANCELLED = 'CANCELLED';
+    const STATUS_DONE = 'DONE';
+
     protected $guarded = [];
 
     public function client()
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CatalogCarController;
 use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\GeneralController;
+use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\SuggestionController;
 use App\Models\Promo;
 use Illuminate\Http\Request;
@@ -32,13 +33,20 @@ Route::prefix('v1')->group(function () {
             Route::get('/get-suggestion', [SuggestionController::class, 'getUserSuggestions'])->name('api.suggestion.get');
         });
 
+        // Client
         Route::get('/client/vehicle', [ClientController::class, 'getClientCars'])->name('api.client.cars');
+
+        // Service
+        Route::group(['prefix' => 'service'], function() {
+            Route::post('booking', [ServiceController::class, 'bookingService'])->name('api.service.booking');
+            Route::post('visit-service', [ServiceController::class, 'visitService'])->name('api.service.visit');
+        });
     });
 
     // Promos
     Route::get('/promos', [GeneralController::class, 'promos'])->name('api.promos');
 
-    // Used Cars
+    // Catalog Car
     Route::get('/used-car', [CatalogCarController::class, 'getUsedCars'])->name('api.catalog-cars.used');
     Route::get('/new-car', [CatalogCarController::class, 'getNewCars'])->name('api.catalog-cars.new');
 });
